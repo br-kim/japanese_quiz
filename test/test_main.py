066636c02963644ebcbb6525699c28f67a27a01e
idle947 <idle947@gmail.com>
2021-05-16 18:46:13 +0900

    테스트 추가

--- test/test_main.py
+++ test/test_main.py
@@ -8,3 +8,17 @@ client = TestClient(app)
 def test_main():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_quiz_path_only_hiragana():
+    payload = {'hiragana': 'hiragana'}
+    response = client.get("/quiz/new", params=payload)
+    json = response.json()
+    assert json['path'].split('/')[3] == 'hiragana'
+
+
+def test_quiz_path_only_katakana():
+    payload = {'katakana': 'katakana'}
+    response = client.get("/quiz/new", params=payload)
+    json = response.json()
+    assert json['path'].split('/')[3] == 'katakana'
