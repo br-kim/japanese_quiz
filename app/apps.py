dbfff9a2652a7c85d9548b431102cc60ae73b8f1
idle947 <idle947@gmail.com>
2021-05-25 20:18:32 +0900

    'test11'

--- app/apps.py
+++ app/apps.py
@@ -63,4 +63,4 @@ async def pathdata(data_type: str, kind: str = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("apps:app", host="0.0.0.0", port=8000)
