943c2a9b5bbe48dd96c1ccc614c3902915b1a492
idle947 <idle947@gmail.com>
2021-06-11 16:59:29 +0900

    데이터베이스 URL변경

--- app/osenv.py
+++ app/osenv.py
@@ -6,4 +6,7 @@ GOOGLE_CLIENT_ID = os.getenv('JPN_QUIZ_GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('JPN_QUIZ_GOOGLE_CLIENT_SECRET')
 
 SESSION_KEY = os.getenv('JPN_QUIZ_SESSION_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL_EDITED')
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
