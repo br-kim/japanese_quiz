1b299f06e06b465d4badb237a2b842be735ee5da
idle947 <idle947@gmail.com>
2021-06-15 22:32:01 +0900

    Score DB 추가중

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 
-import models, schemas
+import models
+import schemas
 
 
 def get_user(db: Session, user_id: int):
@@ -17,3 +18,22 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def get_user_hiragana_score(db: Session, user_id: int):
+    return db.query(models.HiraganaScore).filter(models.HiraganaScore.id == user_id).first()
+
+
+def get_user_katakana_score(db: Session, user_id: int):
+    return db.query(models.KatakanaScore).filter(models.KatakanaScore.id == user_id).first()
+
+
+def create_user_scoreboard(db: Session, user_id: int):
+    db_hiragana_scoreboard = models.HiraganaScore(id=user_id)
+    db_katakana_scoreboard = models.KatakanaScore(id=user_id)
+    db.add(db_hiragana_scoreboard)
+    db.add(db_katakana_scoreboard)
+    db.commit()
+    db.refresh(db_hiragana_scoreboard)
+    db.refresh(db_katakana_scoreboard)
+    return db_hiragana_scoreboard, db_katakana_scoreboard
--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
+import os
+
 from sqlalchemy import Column, Integer, String
 
 from database import Base
+
+hiragana_data = dict.fromkeys([file_name.split('.')[0] for file_name in os.listdir('static/img/hiragana')], 0)
+katakana_data = dict.fromkeys([file_name.split('.')[0] for file_name in os.listdir('static/img/katakana')], 0)
 
 
 class User(Base):
@@ -8,3 +13,19 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
+
+
+class HiraganaScore(Base):
+    __tablename__ = "hiragana"
+
+    id = Column(Integer, primary_key=True, index=True)
+    score = Column(String, server_default=str(hiragana_data))
+
+
+class KatakanaScore(Base):
+    __tablename__ = "katakana"
+
+    id = Column(Integer, primary_key=True, index=True)
+    score = Column(String, server_default=str(hiragana_data))
+
+
