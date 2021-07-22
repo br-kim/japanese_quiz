06997e78c5dd6099b88dc1e5e55de61ee72eb337
idle947 <idle947@gmail.com>
2021-07-22 15:39:40 +0900

    가중치 계산 방식 변경, 세션 토큰 확인 부분 변경

--- app/routers/quiz.py
+++ app/routers/quiz.py
@@ -21,6 +21,7 @@ quiz_router = APIRouter(dependencies=[Depends(check_user)])
 class AnswerRes(BaseModel):
     csrf_token: str
     character: str
+    quiz_type: str
 
 
 @quiz_router.get(urls.inf_quiz_page_url)
@@ -33,7 +34,7 @@ async def new_quiz(request: Request):
     return templates.TemplateResponse("new_quiz.html", {"request": request})
 
 
-@quiz_router.get(urls.QUIZ_DATA_BASE_URL+'/{data_type}')
+@quiz_router.get(urls.QUIZ_DATA_BASE_URL + '/{data_type}')
 async def path_data(request: Request, data_type: str, kind: str = 'all', is_weighted: str = '',
                     db: Session = Depends(get_db)):
     result = utils.gen_img_path_list(kind)
@@ -44,19 +45,24 @@ async def path_data(request: Request, data_type: str, kind: str = 'all', is_weig
             cur_user_email = request.session.get('user_email')
             cur_user = crud.get_user_by_email(db=db, email=cur_user_email)
             weight = []
+            total_score = 0
+            score_values = []
             if kind == 'hiragana' or kind == 'all':
                 hira_score_db = crud.get_user_hiragana_score(db=db, user_id=cur_user.id)
                 hira_score = hira_score_db.score
                 score_dict = json.loads(hira_score)
-                total_score = sum(score_dict.values())
-                weight += [total_score - i for i in score_dict.values()]
+                print(score_dict)
+                score_values += list(score_dict.values())
+                total_score += sum(score_values)
             if kind == 'katakana' or kind == 'all':
                 kata_score_db = crud.get_user_katakana_score(db=db, user_id=cur_user.id)
                 kata_score = kata_score_db.score
                 score_dict = json.loads(kata_score)
-                total_score = sum(score_dict.values())
-                weight += [total_score - i for i in score_dict.values()]
+                total_score += sum(score_dict.values())
+            weight += [total_score - i for i in score_values]
+            print(result, weight)
             img_path = random.choices(result, weight).pop()
+            print(weight[result.index(img_path)], '/', sum(weight))
         else:
             img_path = random.choice(result)
         return {"path": img_path, "csrf_token": csrf_token}
@@ -83,7 +89,11 @@ async def scoreboard(request: Request, db: Session = Depends(get_db)):
 
 @quiz_router.patch('/scoreupdate')
 async def score_update(request: Request, response: AnswerRes, db: Session = Depends(get_db)):
-    if response.csrf_token == request.session.get('csrf_token'):
+    token = request.session.get('csrf_token')
+    if token is not None and response.quiz_type != '/newquiz':
+        request.session.pop('csrf_token')
+        print(request.session)
+    if response.csrf_token == token:
         char_data = response.character.split('/')[-2:]
         char_type = char_data[0]
         char_name = char_data[1].split('.')[0]
@@ -93,5 +103,6 @@ async def score_update(request: Request, response: AnswerRes, db: Session = Depe
         if result is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request")
     else:
+        print(response.csrf_token, request.session.get('csrf_token'))
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Access")
     return Response(status_code=status.HTTP_204_NO_CONTENT)
