45493495332b9ea63c61446a6f54d663be8460c0
idle947 <idle947@gmail.com>
2021-07-25 15:47:54 +0900

    커뮤니티관련 테스트 생성

---
+++ app/test/test_freeboard.py
@@ -0,0 +1,110 @@
+from fastapi import Request
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
+
+import pytest
+
+from ..apps import app
+from ..database import Base
+import dependencies
+import crud
+import models
+
+
+# @pytest.mark.skip(reason="for dependency override")
+def check_user():
+    return True
+
+
+# @pytest.mark.skip(reason="for dependency override")
+
+
+@pytest.fixture(scope="session")
+def session():
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@127.0.0.1"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    conn = engine.connect()
+    conn.execute('commit')
+    try:
+        conn.execute("create database test_db")
+    except:
+        pass
+    finally:
+        conn.close()
+
+    Base.metadata = models.Base.metadata
+    engine = create_engine(SQLALCHEMY_DATABASE_URL + "/test_db")
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    def override_get_db():
+        db = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    client = TestClient(app)
+    app.dependency_overrides[dependencies.check_user] = check_user
+    app.dependency_overrides[dependencies.get_db] = override_get_db
+    db = TestingSessionLocal()
+    crud.create_user(db, models.User(email='idle947@gmail.com'))
+
+    yield (db, client)
+    db.close()
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    conn = engine.connect()
+    conn.execute('commit')
+    conn.execute("drop database test_db with (force)")
+    # Base.metadata.drop_all(bind=engine)
+
+
+# @pytest.fixture(scope="session")
+# def client():
+#     with TestClient(app) as c:
+#         c.get('/for_test')
+#         yield c
+
+# client = TestClient(app)
+# app.dependency_overrides[dependencies.check_user] = test_check_user
+
+
+# app.dependency_overrides[dependencies.get_db] = test_get_db
+@app.get('/for_test')
+async def for_test(request: Request):
+    request.session['user_email'] = 'idle947@gmail.com'
+    return "complete"
+
+
+def test_db_insert_and_get_article(session):
+    session = session[0]
+    fixture = {'id': 1, 'contents': 'hello', 'writer': 'test@test.com', 'title': 'test-title'}
+    article = models.FreeBoard(**fixture)
+    crud.create_article(session, article)
+    article = crud.get_article(db=session, article_num=1)
+    assert article.id == fixture['id'] and article.contents == fixture['contents'] and \
+           article.writer == fixture['writer'] and article.title == fixture['title']
+
+
+def test_db_inset_and_get_comment(session):
+    session = session[0]
+    fixture = {'id': 1, 'contents': 'hello', 'writer': 'test@test.com', 'article_id': 1}
+    comment = models.Comment(**fixture)
+    crud.create_comment(session, comment)
+    comment = crud.get_comment(db=session, comment_id=1)
+    assert comment.id == fixture['id'] and comment.contents == fixture['contents'] and \
+           comment.writer == fixture['writer']
+
+
+def test_api_article(session):
+    # session = session
+    client = session[1]
+    data = {'title': 'hello', 'contents': 'hello'}
+    res = client.get('/for_test')
+    res = client.post('/freeboard/write/article', json=data)
+    assert res.status_code == 201
+    res = client.get('/freeboard/1')
+    print(res.json())
+    assert res
