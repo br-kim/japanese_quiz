9f2ac0d0fed95af5fb4d90fca88b9c0ea26fa69a
br-kim <idle947@gmail.com>
2021-07-26 21:48:52 +0900

    Revert "Develop"

--- app/models.py
+++ app/models.py
@@ -41,15 +41,15 @@ class FreeBoard(TimestampMixin, Base):
     __tablename__ = "freeboard"
 
     id: Column = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    contents = Column(UnicodeText, nullable=False)
-    writer = Column(String, nullable=False)
+    title = Column(String)
+    contents = Column(UnicodeText)
+    writer = Column(String)
 
 
 class Comment(TimestampMixin, Base):
     __tablename__ = "comment"
 
     id: Column = Column(Integer, primary_key=True, index=True)
-    contents = Column(UnicodeText, nullable=False)
-    writer = Column(String, nullable=False)
-    article_id = Column(Integer, index=True, nullable=False)
+    contents = Column(UnicodeText)
+    writer = Column(String)
+    article_id = Column(Integer, index=True)
