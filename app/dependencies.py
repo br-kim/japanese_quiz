1d66aa79644b87c02c5496364b0e2b6ec435d09c
br-kim <idle947@gmail.com>
2023-07-10 17:19:53 +0900

    deps에서 유저 토큰 request에 추가

--- app/dependencies.py
+++ app/dependencies.py
@@ -7,6 +7,7 @@ from utils import get_token_payload
 async def check_user(request: Request):
     token = request.headers.get("Authorization")
     payload = get_token_payload(token)
+    request.state.user_token = payload
     if not payload:
         raise HTTPException(status_code=403)
     return payload
