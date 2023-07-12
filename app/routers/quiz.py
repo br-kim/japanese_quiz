c0d73d215d296871e48dd6252dda04faae425b44
br-kim <idle947@gmail.com>
2023-07-12 14:45:22 +0900

    유저 확인 로직 수정 및 csrf 토큰 제거

--- app/routers/quiz.py
+++ app/routers/quiz.py
@@ -21,7 +21,6 @@ quiz_router = APIRouter()
 
 
 class AnswerRes(BaseModel):
-    csrf_token: str
     character: str
     quiz_type: str
 
@@ -41,10 +40,8 @@ async def test_mode(request: Request):
 async def random_quiz_data(request: Request, kind: str = "all", is_weighted: Optional[str] = None,
                            db: Session = Depends(get_db), token = Depends(check_user)):
     result = utils.gen_img_path_list(kind)
-    csrf_token = base64.b64encode(os.urandom(8)).decode()
-    request.state.csrf_token = csrf_token
     if is_weighted:
-        cur_user_email = request.state.user_email
+        cur_user_email = token.get("user_email")
         cur_user = crud.get_user_by_email(db=db, email=cur_user_email)
         weight = []
         total_score = 0
@@ -66,7 +63,7 @@ async def random_quiz_data(request: Request, kind: str = "all", is_weighted: Opt
         img_path = random.choices(result, weight).pop()
     else:
         img_path = random.choice(result)
-    return {"path": img_path, "csrf_token": csrf_token}
+    return {"path": img_path}
 
 
 @quiz_router.get("/quiz-data/test-mode")
@@ -134,19 +131,14 @@ async def scoreboard_data(request: Request, db: Session = Depends(get_db), token
     return ScoreData(hiragana=json.loads(hira_score.score), katakana = json.loads(kata_score.score))
 
 @quiz_router.patch('/scoreupdate')
-async def score_update(request: Request, response: AnswerRes, db: Session = Depends(get_db)):
-    token = request.state.csrf_token
-    if token and response.quiz_type != '/newquiz':
-        request.state.csrf_token = None
-    if response.csrf_token == token:
-        char_data = response.character.split('/')[-2:]
-        char_type = char_data[0]
-        char_name = char_data[1].split('.')[0]
-        current_user = crud.get_user_by_email(db=db, email=request.state.user_email)
-        user_id = current_user.id
-        result = crud.update_user_scoreboard(db=db, user_id=user_id, char_type=char_type, char_name=char_name)
-        if not result:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request")
-    else:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Access")
+async def score_update(request: Request, response: AnswerRes, db: Session = Depends(get_db), token=Depends(check_user)):
+    char_data = response.character.split('/')[-2:]
+    char_type = char_data[0]
+    char_name = char_data[1].split('.')[0]
+    user_email = token.get("user_email")
+    current_user = crud.get_user_by_email(db=db, email=user_email)
+    user_id = current_user.id
+    result = crud.update_user_scoreboard(db=db, user_id=user_id, char_type=char_type, char_name=char_name)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request")
     return Response(status_code=status.HTTP_204_NO_CONTENT)
