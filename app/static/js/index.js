8fe8c24cf7410b79ac8215845dfb865738d5ac9e
br-kim <idle947@gmail.com>
2023-07-12 14:46:18 +0900

    프론트 서버 요청용 함수 body도 받도록 수정

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,15 +2,18 @@ let tokenHeader = {'Content-Type': 'application/json',
         'Authorization' : localStorage.getItem("jpn_quiz_access_token")};
 export let serverBaseUrl = window.location.origin;
 
-export async function requestToServer(url, method, is_auth=true){
+export async function requestToServer(url, method,is_auth=true, body=null){
         if ((is_auth) && (!tokenHeader)) {
             alert("토큰 없음");
             return;
         }
         let header = is_auth ? tokenHeader : {};
-        let res = await fetch(url,{
-            headers: header,
-            method: method
-        });
+        let requestInit = {};
+        requestInit.headers = header;
+        requestInit.method = method;
+        if (body){
+            requestInit.body = body;
+        }
+        let res = await fetch(url, requestInit);
         return res;
        }
