774039c0cedc5557cd1e2d11d7ca98aca5e0259e
br-kim <idle947@gmail.com>
2023-07-13 19:56:28 +0900

    랜덤 퀴즈 토큰 없이도 사용할 수 있도록 프론트 요청 코드 수정

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,19 +1,27 @@
-let tokenHeader = {'Content-Type': 'application/json',
-        'Authorization' : localStorage.getItem("jpn_quiz_access_token")};
+let tokenHeader = {
+    'Content-Type': 'application/json',
+};
+
+export let token = localStorage.getItem("jpn_quiz_access_token");
+
+if (token) {
+    tokenHeader['Authorization'] = localStorage.getItem("jpn_quiz_access_token");
+}
+
 export let serverBaseUrl = window.location.origin;
 
-export async function requestToServer(url, method,is_auth=true, body=null){
-        if ((is_auth) && (!tokenHeader)) {
-            alert("토큰 없음");
-            return;
-        }
-        let header = is_auth ? tokenHeader : {};
-        let requestInit = {};
-        requestInit.headers = header;
-        requestInit.method = method;
-        if (body){
-            requestInit.body = body;
-        }
-        let res = await fetch(url, requestInit);
-        return res;
-       }
+export async function requestToServer(url, method, is_auth = true, body = null) {
+    if ((is_auth) && (!token)) {
+        alert("토큰 없음");
+        return;
+    }
+    let header = token ? tokenHeader : {};
+    let requestInit = {};
+    requestInit.headers = header;
+    requestInit.method = method;
+    if (body) {
+        requestInit.body = body;
+    }
+    let res = await fetch(url, requestInit);
+    return res;
+}
