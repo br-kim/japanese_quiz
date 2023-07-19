8f392857d9d9aaae39f878049cb8a9acefbfcc8f
br-kim <idle947@gmail.com>
2023-07-19 14:06:58 +0900

    로컬 스토리에 로그인 유저 이메일 저장 추가

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -85,7 +85,7 @@ export async function generateNav() {
             // localStorage.removeItem("jpn_quiz_access_token");
         }
         let user_email = await res.json();
-        console.log(user_email);
+        localStorage.setItem("user_email", user_email);
         let scoreBoardNav = createAElement(navMenuRight, "/scoreboard", `${user_email}님, 환영합니다!`);
         navDiv.appendChild(scoreBoardNav);
         navDiv.appendChild(logoutNav);
