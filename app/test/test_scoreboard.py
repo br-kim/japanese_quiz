71ac788f0f809f356963bf7e3c09f33b8474c244
br-kim <idle947@gmail.com>
2023-07-21 20:14:57 +0900

    값 업데이트 함수 테스트 추가

--- app/test/test_scoreboard.py
+++ app/test/test_scoreboard.py
@@ -6,3 +6,16 @@ def test_get_scoreboard_data(client, create_score):
     assert response.status_code == 200
     assert len(json) == 2
     assert json.get("hiragana") and json.get("katakana")
+
+
+def test_update_scoreboard_data(client, create_score):
+    token = create_score
+    header = {'Authorization': f'Bearer {token}'}
+    body = dict(character="katakana/ka.png", quiz_type="katakana")
+    response = client.patch('/scoreboard/data', json=body, headers=header)
+    assert response.status_code == 204
+    response = client.get('/scoreboard/data', headers=header)
+    json = response.json()
+    assert response.status_code == 200
+    assert json.get("hiragana") and json.get("katakana")
+    assert json.get("katakana").get("ka") == 1
