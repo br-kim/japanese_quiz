ae5e59863c3fc2db15328a15678ecfe72a537154
br-kim <idle947@gmail.com>
2023-07-21 18:41:48 +0900

    테스트 환경 용 디비 세팅 추가

--- app/osenv.py
+++ app/osenv.py
@@ -6,7 +6,13 @@ GOOGLE_CLIENT_ID = os.getenv('JPN_QUIZ_GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('JPN_QUIZ_GOOGLE_CLIENT_SECRET')
 
 JWT_KEY = os.getenv('JPN_QUIZ_JWT_KEY', "secret-key")
-DATABASE_URL = os.getenv('DATABASE_URL', default="postgresql://ii:1234@localhost:5432/japanese_quiz")
+ENVIRON = os.getenv('JPN_QUIZ_ENVIRON', "dev")
+
+if ENVIRON == "test":
+    DATABASE_URL = os.getenv('DATABASE_URL', default="postgresql://ii:1234@localhost:5432/test_database")
+else:
+    DATABASE_URL = os.getenv('DATABASE_URL', default="postgresql://ii:1234@localhost:5432/japanese_quiz")
+
 POSTGRES_DATABASE_URL = os.getenv('POSTGRES_DATABASE_URL', default="postgresql://postgres:postgres@localhost:5432/postgres")
 TEST_DATABASE_URL = os.getenv('TEST_DATABASE_URL', default="postgresql://ii:1234@localhost:5432/test_database")
 
