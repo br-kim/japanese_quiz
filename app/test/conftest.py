686704bfb2f38d6a2ff8ed6e3df344bfbdad9fe7
br-kim <idle947@gmail.com>
2023-07-21 15:18:08 +0900

    테스트 세팅 다른 코드 보고 참고하여 수정

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,43 +1,87 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, drop_database, create_database
 from sqlalchemy import text
+from fastapi.testclient import TestClient
+
 
 import osenv
+import models
 from models import Base
+from utils import create_token
+from .database import Session
 
+# def create_database():
+#     engine = create_engine(osenv.POSTGRES_DATABASE_URL)
+#     conn = engine.connect()
+#     conn.execute(text("commit"))
+#     try:
+#         conn.execute(text("CREATE DATABASE test_database"))
+#     except:
+#         pass
 
-def create_database():
-    engine = create_engine(osenv.POSTGRES_DATABASE_URL)
-    conn = engine.connect()
-    conn.execute(text("commit"))
-    try:
-        conn.execute(text("CREATE DATABASE test_database"))
-    except:
-        pass
+def init_database():
+    if not database_exists(str(osenv.TEST_DATABASE_URL)):
+        create_database(str(osenv.TEST_DATABASE_URL))
+
 
 @pytest.fixture(scope="session")
-def session():
-    # engine = create_engine(SQLALCHEMY_DATABASE_URL)
-    # conn = engine.connect()
-    # conn.execute('commit')
-    # try:
-    #     conn.execute("create database test_db")
-    # except:
-    #     pass
-    # finally:
-    #     conn.close()
-    create_database()
-    engine = create_engine(osenv.TEST_DATABASE_URL)
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    Base.metadata.drop_all(bind=engine)
+def db():
+    if database_exists(str(osenv.TEST_DATABASE_URL)):
+        drop_database(str(osenv.TEST_DATABASE_URL))
+    init_database()
+    engine = create_engine(str(osenv.TEST_DATABASE_URL))
     Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    yield db
-    db.close()
+    Session.configure(bind=engine)
+    yield
+    drop_database(str(osenv.TEST_DATABASE_URL))
 
-    # engine = create_engine(SQLALCHEMY_DATABASE_URL)
-    # conn = engine.connect()
-    # conn.execute('commit')
-    # conn.execute("drop database test_db with (force)")
 
+@pytest.fixture(scope="function", autouse=True)
+def session(db):
+    session = Session()
+    session.begin_nested()
+    yield session
+    session.rollback()
+
+@pytest.fixture(scope="function")
+def client(session):
+    from apps import app
+    yield TestClient(app)
+
+@pytest.fixture(scope="function")
+def create_user(client, session):
+    user_email = "test@test.com"
+    user_name = "test"
+    new_user = models.User(email=user_email)
+    session.add(new_user)
+    payload = dict(user_email=user_email, user_name=user_name, user_id=new_user.id)
+    token = create_token(payload=payload)
+    return token
+
+
+# @pytest.fixture(scope="session")
+# def session():
+#     # engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#     # conn = engine.connect()
+#     # conn.execute('commit')
+#     # try:
+#     #     conn.execute("create database test_db")
+#     # except:
+#     #     pass
+#     # finally:
+#     #     conn.close()
+#     create_database()
+#     engine = create_engine(osenv.TEST_DATABASE_URL)
+#     TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#     Base.metadata.drop_all(bind=engine)
+#     Base.metadata.create_all(bind=engine)
+#     db = TestingSessionLocal()
+#     yield db
+#     db.close()
+#
+#     # engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#     # conn = engine.connect()
+#     # conn.execute('commit')
+#     # conn.execute("drop database test_db with (force)")
