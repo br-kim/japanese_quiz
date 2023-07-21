b4eb4776840c74cc3491ca1c0461e7f7cfdd4c93
br-kim <idle947@gmail.com>
2023-07-21 15:18:38 +0900

    퀴즈 테스트 케이스 추가 및 기존 코드 수정

--- app/test/test_quiz.py
+++ app/test/test_quiz.py
@@ -1,35 +1,66 @@
-from fastapi.testclient import TestClient
 
-from ..apps import app
-import dependencies
-
-
-def check_user():
-    return True
-
-
-app.dependency_overrides[dependencies.check_user] = check_user
-
-client = TestClient(app)
-
-
-def test_get_quiz_path_only_hiragana():
+def test_get_quiz_path_only_hiragana(client):
+    """
+    랜덤 퀴즈 히라가나 데이터 요청 테스트
+    """
     payload = {'kind': 'hiragana'}
-    response = client.get('/quizdata/path', params=payload)
+    response = client.get('/quiz-data/random', params=payload)
     json = response.json()
     assert json['path'].split('/')[3] == 'hiragana'
 
 
-def test_get_quiz_path_only_katakana():
+def test_get_quiz_path_only_katakana(client):
+    """
+    랜덤 퀴즈 카타카나 데이터 요청 테스트
+    """
     payload = {'kind': 'katakana'}
-    response = client.get('/quizdata/path', params=payload)
+    response = client.get('/quiz-data/random', params=payload)
     json = response.json()
     assert json['path'].split('/')[3] == 'katakana'
 
 
-def test_get_quiz_path_both():
+def test_get_quiz_path_both(client):
+    """
+    랜덤 퀴즈 모든 글자 데이터 요청 테스트
+    """
     payload = {'kind': 'all'}
-    response = client.get('/quizdata/path', params=payload)
+    response = client.get('/quiz-data/random', params=payload)
     json = response.json()
     gana_type = json['path'].split('/')[3]
-    assert gana_type == 'katakana' or gana_type == 'hiragana'
+    assert gana_type in ['hiragana', "katakana"]
+
+
+def test_get_test_mode_data(client, create_user):
+    """
+    테스트 모드 데이터 요청 테스트
+    """
+    token = create_user
+    payload = {'kind': 'all'}
+    header = {'Authorization': f'Bearer {token}'}
+    response = client.get('/quiz-data/test-mode',headers=header, params=payload)
+    json = response.json()
+    assert len(json['order']) == 208
+
+def test_get_test_mode_data_hiragana(client, create_user):
+    """
+    테스트 모드 히라가나 데이터 요청 테스트
+    """
+    token = create_user
+    payload = {'kind': 'hiragana'}
+    header = {'Authorization': f'Bearer {token}'}
+    response = client.get('/quiz-data/test-mode',headers=header, params=payload)
+    json = response.json()
+    assert len(json['order']) == 104
+
+def test_get_test_mode_data_katakana(client, create_user):
+    """
+    테스트 모드 카타카나 데이터 요청 테스트
+    """
+    token = create_user
+    payload = {'kind': 'katakana'}
+    header = {'Authorization': f'Bearer {token}'}
+    response = client.get('/quiz-data/test-mode',headers=header, params=payload)
+    json = response.json()
+    assert len(json['order']) == 104
+    
+
