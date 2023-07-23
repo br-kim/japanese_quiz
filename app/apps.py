117ce75a1b6985b67f6c8b65a19a5ed453c9fcdf
br-kim <idle947@gmail.com>
2023-07-23 18:31:21 +0900

    필요없는 코드 제거

--- app/apps.py
+++ app/apps.py
@@ -1,7 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 import osenv
 from routers import quiz, login, community, chatting, user, scoreboard, index
