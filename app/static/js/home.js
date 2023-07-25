0d02d859342cb1378a52b4c9c59f20c85ed28bad
br-kim <idle947@gmail.com>
2023-07-25 15:39:10 +0900

    프론트 navbar 수정

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,13 +1,13 @@
 import {requestToServer, serverBaseUrl} from "./index.js";
 
 
-export async function loginFunction(){
-    let login_url = await requestToServer(serverBaseUrl+"/login", "GET", false);
+export async function loginFunction() {
+    let login_url = await requestToServer(serverBaseUrl + "/login", "GET", false);
     let url = await login_url.json();
     window.location.href = url;
 }
 
-export async function usingCode(){
+export async function usingCode() {
     // 현재 페이지의 URL
     const url = new URL(window.location.href);
 
@@ -24,94 +24,79 @@ export async function usingCode(){
 
     let res = await requestToServer(apiUrl, "GET", false);
     let token = await res.json();
-    localStorage.setItem("jpn_quiz_access_token",token);
+    localStorage.setItem("jpn_quiz_access_token", token);
     window.location.href = serverBaseUrl;
 }
 
 export async function generateNav() {
-    function createAElement(className, href, text, id=null){
+    function createAElement(href, text, id = null) {
         let element = document.createElement("a");
-        if (className){
-            // element.classList.add(className);
-            element.classList.add("nav-link");
-            element.classList.add("active");
-        }
-        if (href){
+        element.classList.add("nav-link");
+        element.classList.add("active");
+        if (href) {
             element.href = href;
         }
-        if (text){
+        if (text) {
             element.textContent = text;
         }
-        if (id){
+        if (id) {
             element.id = id;
         }
-        // create li element class nav-item
-        let liElement = document.createElement("li");
-        liElement.classList.add("nav-item");
-        liElement.appendChild(element);
-        return liElement;
+        let divElement = document.createElement("div");
+        divElement.classList.add("nav-item");
+        divElement.appendChild(element);
+        return divElement;
     }
-    let navMenuRight = "nav-menu-right";
-    let navMenu = "nav-menu";
 
     let navDiv = document.getElementById("nav-div");
 
-    let mainNav = createAElement(navMenu, "/", "메인");
-    let infNav = createAElement(navMenu, "/quiz", "무한모드");
-    let testNav = createAElement(navMenu, "/newquiz", "테스트");
-    let freeboardNav = createAElement(navMenu, "/fb", "자유게시판");
-    let chattingNav = createAElement(navMenu, "/ws", "채팅");
-    let loginNav = createAElement(navMenuRight, null, "로그인", "nav-login");
-    let logoutNav = createAElement(navMenuRight, "/logout", "로그아웃");
+    let navLeftDiv = document.createElement("div");
+    let navRightDiv = document.createElement("div");
+
+    navLeftDiv.classList.add("d-flex", "justify-content-start");
+    navRightDiv.classList.add("d-flex", "justify-content-end");
+
+    let mainNav = createAElement("/", "메인");
+    let infNav = createAElement("/quiz", "무한모드");
+    let testNav = createAElement("/newquiz", "테스트");
+    let freeboardNav = createAElement("/fb", "자유게시판");
+    let chattingNav = createAElement("/ws", "채팅");
+    let loginNav = createAElement(null, "로그인", "nav-login");
+    let logoutNav = createAElement("/logout", "로그아웃");
     logoutNav.addEventListener("click", () => {
         localStorage.removeItem("jpn_quiz_access_token");
     });
-    // create ul element class navbar-nav me-auto mb-2 mb-lg-0
-    let ulElement = document.createElement("ul");
-    navDiv.appendChild(ulElement);
+    let divContainer = document.createElement("div");
 
-    ulElement.classList.add("navbar-nav", "me-auto", "mb-2", "mb-lg-0");
-    ulElement.appendChild(mainNav);
-    ulElement.appendChild(infNav);
-    ulElement.appendChild(testNav);
-    ulElement.appendChild(freeboardNav);
-    ulElement.appendChild(chattingNav);
+    navDiv.appendChild(divContainer);
 
-    // navDiv.appendChild(mainNav);
-    // navDiv.appendChild(infNav);
-    // navDiv.appendChild(testNav);
-    // navDiv.appendChild(freeboardNav);
-    // navDiv.appendChild(chattingNav);
+    divContainer.classList.add("navbar-nav", "mb-2", "mb-lg-0", "w-100", "justify-content-between");
+    divContainer.append(navLeftDiv, navRightDiv);
+
+    navLeftDiv.append(mainNav, infNav, testNav, freeboardNav, chattingNav);
 
     if (localStorage.getItem("jpn_quiz_access_token")) {
-        let res = await requestToServer(serverBaseUrl+"/user-info", "GET");
-        if (res.status === 403){
+        let res = await requestToServer(serverBaseUrl + "/user-info", "GET");
+        if (res.status === 403) {
             let errorMsg = await res.json();
-            if (errorMsg.detail === "Token Expire"){
+            if (errorMsg.detail === "Token Expire") {
                 alert("로그인이 만료되었습니다.");
                 localStorage.removeItem("jpn_quiz_access_token");
                 loginNav.addEventListener("click", loginFunction);
-                ulElement.appendChild(loginNav);
-                // navDiv.append(loginNav);
+                navRightDiv.appendChild(loginNav);
                 return;
-            }
-            else{
+            } else {
                 alert("처리되지 않은 에러입니다.");
             }
             // localStorage.removeItem("jpn_quiz_access_token");
         }
         let userInfoRes = await res.json();
         localStorage.setItem("user_email", userInfoRes.email);
-        let scoreBoardNav = createAElement(navMenuRight, "/scoreboard", `${userInfoRes.email}님, 환영합니다!`);
-        ulElement.appendChild(scoreBoardNav);
-        ulElement.appendChild(logoutNav);
-
-        // navDiv.appendChild(scoreBoardNav);
-        // navDiv.appendChild(logoutNav);
-    }
-    else{
+        let scoreBoardNav = createAElement("/scoreboard", `${userInfoRes.email}님, 환영합니다!`);
+        navRightDiv.appendChild(scoreBoardNav);
+        navRightDiv.appendChild(logoutNav);
+    } else {
         loginNav.addEventListener("click", loginFunction);
-        ulElement.appendChild(loginNav);
-        // navDiv.append(loginNav);
+        navRightDiv.appendChild(loginNav);
     }
 }
