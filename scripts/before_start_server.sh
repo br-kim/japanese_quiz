a54b78404d24ddcf4e6d7e55cc17e3253988b7e2
br-kim <idle947@gmail.com>
2023-07-27 23:09:15 +0900

    서비스들 시작하도록 변경

--- scripts/before_start_server.sh
+++ scripts/before_start_server.sh
@@ -1,7 +1,20 @@
 python3.11 -m venv /home/ec2-user/build/app/venv
 echo "create venv"
+
 source /home/ec2-user/build/app/venv/bin/activate
 echo "activate venv"
+
 pip install -r /home/ec2-user/build/app/requirements.txt
 echo "install requirements"
 
+sudo systemctl start postgresql
+echo "start postgresql"
+
+sudo systemctl enable postgresql
+echo "enable postgresql"
+
+sudo systemctl start redis6
+echo "start redis6"
+
+sudo systemctl enable redis6
+echo "enable redis6"
