d0da786cb1651a78c67358c22af0b025ee1f9331
br-kim <idle947@gmail.com>
2023-07-27 22:24:01 +0900

    script 위치 변경

--- scripts/before_start_server.sh
+++ scripts/before_start_server.sh
@@ -4,7 +4,4 @@ source /home/ec2-user/build/app/venv/bin/activate
 echo "activate venv"
 pip install -r /home/ec2-user/build/app/requirements.txt
 echo "install requirements"
-export PATH=$PATH:$HOME/build/app/venv/lib/python3.9/site-packages
-echo "export PATH"
-source ~/.bashrc
-echo "source bashrc"
+
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,1 +1,6 @@
+export PATH=$PATH:$HOME/build/app/venv/lib/python3.9/site-packages
+echo "export PATH"
+source ~/.bashrc
+echo "source bashrc"
+
 gunicorn -b :8000 apps:app -k uvicorn.workers.UvicornWorker
