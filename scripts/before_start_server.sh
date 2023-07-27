6e3f195245431e3f55bfa2fc7f396ebc87fa25ec
br-kim <idle947@gmail.com>
2023-07-27 22:09:19 +0900

    venv 사용하도록 수정

--- scripts/before_start_server.sh
+++ scripts/before_start_server.sh
@@ -1,2 +1,6 @@
+python3 -m venv /home/ec2-user/build/app/venv
+echo "create venv"
+source /home/ec2-user/build/app/venv/bin/activate
+echo "activate venv"
 pip install -r /home/ec2-user/build/app/requirements.txt
 echo "install requirements"
