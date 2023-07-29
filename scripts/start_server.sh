d0db65cf13134ce1187f83574001aa631aec12a0
br-kim <idle947@gmail.com>
2023-07-29 14:54:36 +0900

    디렉토리 생성하는 스크립트 추가

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -11,6 +11,13 @@ echo "cd app"
 echo $DATABASE_URL
 echo "check DATABASE_URL"
 
+log_dir="/home/ec2-user/log"
+
+# 디렉토리가 존재하는지 확인하고 없으면 생성
+if [ ! -d "$log_dir" ]; then
+  mkdir -p "$log_dir"
+fi
+
 current_date=$(date +"%Y%m%dT%H%M%S")
 gunicorn -b :8000 apps:app -k uvicorn.workers.UvicornWorker > \
  /home/ec2-user/log/out_$current_date 2> /home/ec2-user/log/err_$current_date < /dev/null &
