836b81d9a2f8877a95a6604b73d9f9e169a8a1ca
br-kim <idle947@gmail.com>
2023-08-02 22:30:49 +0900

    docs 운영 서버에 보이지 않도록 설정

--- app/config.py
+++ app/config.py
@@ -6,16 +6,19 @@ from pydantic_settings import BaseSettings
 class TestSettings(BaseSettings):
     DATABASE_URL: str = os.getenv('DATABASE_URL', default="postgresql://ii:1234@localhost:5432/test_database")
     SERVER_URL: str = "http://localhost:8000"
+    DOCS_SETTING: dict = dict(openapi_url = None, docs_url = None, redoc_url = None)
 
 
 class LocalSettings(BaseSettings):
     DATABASE_URL: str = os.getenv('DATABASE_URL', default="postgresql://ii:1234@localhost:5432/japanese_quiz")
     SERVER_URL: str = "http://localhost:8000"
+    DOCS_SETTING: dict = dict(openapi_url = "/openapi.json", docs_url = "/docs", redoc_url = "/redoc")
 
 
 class ProductionSettings(BaseSettings):
     DATABASE_URL: str = os.getenv('DATABASE_URL', default="postgresql://ii:1234@localhost:5432/japanese_quiz")
     SERVER_URL: str = "https://japanese-quiz.site"
+    DOCS_SETTING: dict = dict(openapi_url = None, docs_url = None, redoc_url = None)
 
 
 @lru_cache()
