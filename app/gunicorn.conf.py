682179d8ccf023137f2a75120a44dbaf0bd8d403
br-kim <idle947@gmail.com>
2023-08-06 17:14:59 +0900

    환경변수로 날짜 전달하도록 수정

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,7 +1,8 @@
 import constants
+import os
 
 bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
 workers = 2
-accesslog = '/path/to/access.log'
-errorlog = '/path/to/error.log'
+accesslog = f'/home/ec2-user/log/{os.getenv("JPN_QUIZ_DEPLOY_DATE")}/out.log'
+errorlog = f'/home/ec2-user/log/{os.getenv("JPN_QUIZ_DEPLOY_DATE")}/err.log'
