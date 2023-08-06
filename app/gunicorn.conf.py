c493a5e136f805128e75cf4686708a750ac58bd8
br-kim <idle947@gmail.com>
2023-08-06 19:08:58 +0900

    worker 수 수정

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -6,6 +6,5 @@ for key, value in os.environ.items():
 
 bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
-workers = 2
 accesslog = f'/home/ec2-user/log/{os.getenv("JPN_QUIZ_DEPLOY_DATE")}/out.log'
 errorlog = f'/home/ec2-user/log/{os.getenv("JPN_QUIZ_DEPLOY_DATE")}/err.log'
