f1995f315accaff185e9c4ab96e00b441d98bb85
br-kim <idle947@gmail.com>
2023-08-07 14:07:06 +0900

    gunicorn 설정 수정3

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -8,6 +8,6 @@ bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
 # access_log = constants.LOG_PATH
 # error_log = constants.ERROR_LOG_PATH
-logfile = constants.LOG_PATH
+logfile = constants.LOG_PATH+"gunicorn.log"
 loglevel = 'info'
 capture_output = True
