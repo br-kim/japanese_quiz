40bc24c04811dcdb51efff9bef34b6d37ae8351f
br-kim <idle947@gmail.com>
2023-08-07 13:29:57 +0900

    gunicorn 설정 추가

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -8,3 +8,5 @@ bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
 access_log = constants.LOG_PATH
 error_log = constants.ERROR_LOG_PATH
+loglevel = 'info'
+capture_output = True
