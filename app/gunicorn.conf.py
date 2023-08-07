b00be41942ccec82f90d33d35d04adac95d9b3c3
br-kim <idle947@gmail.com>
2023-08-07 16:59:34 +0900

    로깅 관련 설정 수정

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -2,7 +2,6 @@ import constants
 
 bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
-accesslogformat = '%(h)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 accesslog = constants.LOG_PATH
 errorlog = constants.ERROR_LOG_PATH
 loglevel = 'info'
