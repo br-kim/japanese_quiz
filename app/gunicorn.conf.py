91571767838dc83a0cd80e7a0f3a2067c255a92a
br-kim <idle947@gmail.com>
2023-08-07 15:04:25 +0900

    gunicorn 설정 stdio 상속 추가

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -4,6 +4,6 @@ bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
 access_log = constants.LOG_PATH
 error_log = constants.ERROR_LOG_PATH
-logfile = constants.LOG_PATH+".logfile"
 loglevel = 'info'
 capture_output = True
+enable_stdio_inheritance = True
