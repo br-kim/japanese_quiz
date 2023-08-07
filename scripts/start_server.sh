09f731df3b91b31e3124680b50d82d52e4f40db5
br-kim <idle947@gmail.com>
2023-08-07 15:14:39 +0900

    gunicorn 설정 파일 명시 추가

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -31,7 +31,7 @@ fi
 
 cd /home/ec2-user/build/app
 
-gunicorn apps:app > $log_dir/$current_date/out 2> $log_dir/$current_date/err < /dev/null &
+gunicorn apps:app -c gunicorn.conf.py> $log_dir/$current_date/out 2> $log_dir/$current_date/err < /dev/null &
 
 echo "start server"
 
