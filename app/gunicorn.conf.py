40d09c10fe367ca71b25f5da9e28b6224b71599e
br-kim <idle947@gmail.com>
2023-08-07 14:41:41 +0900

    gunicorn 설정 재수정1

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,10 +1,8 @@
 import constants
-import os
 
 bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
-# access_log = constants.LOG_PATH
-# error_log = constants.ERROR_LOG_PATH
-logfile = constants.LOG_PATH
+access_log = constants.LOG_PATH
+error_log = constants.ERROR_LOG_PATH
 loglevel = 'info'
 capture_output = True
