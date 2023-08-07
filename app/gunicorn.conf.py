81ece532973407e80edca59fb9559044d055f5ab
br-kim <idle947@gmail.com>
2023-08-07 15:24:17 +0900

    gunicorn 로그 포맷 바꿔보기

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -7,3 +7,4 @@ error_log = constants.ERROR_LOG_PATH
 loglevel = 'info'
 capture_output = True
 enable_stdio_inheritance = True
+access_log_format = '%({x-real-ip}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
