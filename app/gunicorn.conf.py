f6574d08603d2941c49e0e6f4a06c42b17cb5c06
br-kim <idle947@gmail.com>
2023-08-07 16:01:20 +0900

    gunicorn 로그 포맷 변경 시도

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -2,9 +2,9 @@ import constants
 
 bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
-accesslog = constants.LOG_PATH+".access_log"
-errorlog = constants.ERROR_LOG_PATH+".error_log"
+access_log_format = '%({x-real-ip}i)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+accesslog = constants.LOG_PATH
+errorlog = constants.ERROR_LOG_PATH
 loglevel = 'info'
 capture_output = True
 enable_stdio_inheritance = True
-access_log_format = '%({x-real-ip}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
