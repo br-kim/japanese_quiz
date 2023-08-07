c482c74696be2e3af42181c9d9be01c785b9996c
br-kim <idle947@gmail.com>
2023-08-07 18:11:46 +0900

    로깅 관련 설정 재수정8

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -2,9 +2,9 @@ import constants
 
 bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
-accesslog = constants.LOG_PATH
-errorlog = constants.ERROR_LOG_PATH
-accesslog_format = '%(h)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-loglevel = 'debug'
+# accesslog = constants.LOG_PATH
+# errorlog = constants.ERROR_LOG_PATH
+# accesslog_format = '%(h)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+# loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
