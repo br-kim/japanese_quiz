62ef466fe5825d1c3e020fe5f4ba7652309d0278
br-kim <idle947@gmail.com>
2023-08-07 15:36:08 +0900

    gunicorn 로그 저장 위치 변경해보기

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -2,8 +2,8 @@ import constants
 
 bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
-access_log = constants.LOG_PATH
-error_log = constants.ERROR_LOG_PATH
+access_log = constants.LOG_PATH+".access_log"
+error_log = constants.ERROR_LOG_PATH+".error_log"
 loglevel = 'info'
 capture_output = True
 enable_stdio_inheritance = True
