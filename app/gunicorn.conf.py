cc66294ee72a01a3843c54af81349c9bf36908eb
br-kim <idle947@gmail.com>
2023-08-08 11:47:39 +0900

    로깅 관련 설정 복구

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -2,8 +2,5 @@ import constants
 
 bind = '0.0.0.0:' + str(constants.PORT_NUMBER)
 worker_class = 'uvicorn.workers.UvicornWorker'
-# accesslog = constants.LOG_PATH
-# errorlog = constants.ERROR_LOG_PATH
-# loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
