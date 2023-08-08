e7b008a3ce80b155fb564b03e3c828dc181f364c
br-kim <idle947@gmail.com>
2023-08-08 17:28:49 +0900

    pydantic 버전 수정에 따른 변경

--- app/schemas/chatting.py
+++ app/schemas/chatting.py
@@ -3,7 +3,7 @@ from typing import List
 from pydantic import BaseModel
 
 class MessageEvent(BaseModel):
-    sender: str = None
-    message: str | List[str]
+    sender: str | None = None
+    message: str | List[str] = None
     message_type: str
-    receiver: str = None
+    receiver: str | None = None
